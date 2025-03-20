3560d10f68ad62c25ccec64b17c124344893cf29
DEEPAK-KUMAR-5 <154720833+DEEPAK-KUMAR-5@users.noreply.github.com>
2025-03-20 18:28:08 +0530

    Update automail.py

--- automail.py
+++ automail.py
@@ -1,14 +1,28 @@
-import pandas as pd
-import os as os
+import pandas
 import smtplib
-df = pd.read_csv("automail.csv")
-second_column = df.iloc[:, 1].tolist()
-server= smtplib.SMTP('smtp.gmail.com', 587)
+import os
+from email.message import EmailMessage
+
+mainfile = pandas.read_csv("automail.csv")
+email_list = mainfile.iloc[:, 1].tolist()
+id_list = mainfile.iloc[:, 2].tolist()
+server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login("dk020402007@gmail.com", "poqk opgo lems svcn")
-message = "hi this ia an automated mail from team GDG"
-for i in second_column:
-    server.sendmail("dk020402007@gmail.com",i, message)
-    print("mail sent to f{i}")
+server.login("xxxxxxxxxxxxxxxxx@gmail.com", "poqk opgo lems svcn")
+subject = "xxxxxxxxxxxxxxxxxxxxxxxxx"
+body = "xxxxxxxxxxxxxxxxxxxxxxxxx"
+for email, ids in zip(email_list, id_list):
+    certificate_file = fr"D:\python\{ids}.pdf" #depends upon the file location
+    msg = EmailMessage()
+    msg['From'] = "xxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    msg['To'] = email
+    msg['Subject'] = subject
+    msg.set_content(body)
+    if os.path.exists(certificate):
+        with open(certificate_file, "rb") as f:
+            msg.add_attachment(f.read(), maintype="application", subtype="pdf", filename=os.path.basename(certificate))
+        server.send_message(msg)
+        print(f"Certificate sent to {email}")
+    else:
+        print(f"Certificate not found for {email} ({certificate})")
 server.quit()
-    
